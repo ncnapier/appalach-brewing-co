b2a60681ad4f15df2818da2e66c6adf26392c93c
Nathaniel C Napier <nattydevs@gmail.com>
2024-01-17 17:44:07 -0600

    first commit- all files

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/components/about.css'
+import '../styles/components/team.css'
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
+
+
