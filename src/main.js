79573fae214a5818e8c89448cc27ac3bce343544
Nathaniel C Napier <nattydevs@gmail.com>
2024-01-19 13:21:56 -0600

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css'
+import '../styles/components/hero.css'
 import '../styles/components/mobile-nav.css'
 import '../styles/components/about.css'
 import '../styles/components/team.css'
