806a485ac92c08288c42845e37465422a452588d
Nathaniel C Napier <nattydevs@gmail.com>
2024-01-18 20:05:27 -0600

    starting beers section

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/components/header.css'
 import '../styles/components/mobile-nav.css'
 import '../styles/components/about.css'
 import '../styles/components/team.css'
+import '../styles/components/beers.css'
 import '../styles/utils.css';
 
 import mobileNav from './utils/mobile-nav';
